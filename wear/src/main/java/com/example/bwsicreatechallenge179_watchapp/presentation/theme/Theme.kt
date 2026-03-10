4f31eaae7013c5f90d52b0b6e5893f2d7a280b88
Danchaper <146667449+Danchaper@users.noreply.github.com>
2026-03-09 20:47:31 -0400

    Initial commit

---
+++ wear/src/main/java/com/example/bwsicreatechallenge179_watchapp/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.bwsicreatechallenge179_watchapp.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun BWSICreateChallenge179WatchAppTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
